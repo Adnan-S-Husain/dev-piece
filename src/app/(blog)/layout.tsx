a087b8324e633d71086d10ce39436b8d85b6a3a6
Adnan Shabbir Husain <generaladnan139@gmail.com>
2023-10-02 23:08:11 +0530

    update: metadata

--- src/app/(blog)/layout.tsx
+++ src/app/(blog)/layout.tsx
@@ -9,12 +9,12 @@ import "../globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Weekly Blogs: Tech Insights, Tutorials, and How-to Advice",
+  title: "Dev Piece: Tech Insights, Tutorials, and How-to Advice",
   description:
     "Discover tech insights, tutorials, and how-to guides on our blog. Stay updated with the latest in software development, full-stack expertise, web, and mobile technologies.",
   keywords: "blog, software, tech, development, full stack, web, mobile",
   openGraph: {
-    title: "Weekly Blogs: Tech Insights, Tutorials, and How-to Advice",
+    title: "Dev Piece: Tech Insights, Tutorials, and How-to Advice",
     description:
       "Discover tech insights, tutorials, and how-to guides on our blog. Stay updated with the latest technologies.",
     images: "/Logo.svg",
