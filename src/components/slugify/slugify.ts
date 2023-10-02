ea28e8a3a242f13172f9de4f8c7df9de02626395
Adnan Shabbir Husain <generaladnan139@gmail.com>
2023-10-02 22:08:36 +0530

    fix: slugify string error

--- src/components/slugify/slugify.ts
+++ src/components/slugify/slugify.ts
@@ -1,5 +1,9 @@
 export default function slugify(input: string): string {
-  const stringWithHyphens = input.replace(/\s+/g, "-");
-  const alphanumericString = stringWithHyphens.replace(/[^a-zA-Z0-9-]/g, "");
-  return alphanumericString.toLocaleLowerCase();
+  try {
+    const stringWithHyphens = input.replace(/\s+/g, "-");
+    const alphanumericString = stringWithHyphens.replace(/[^a-zA-Z0-9-]/g, "");
+    return alphanumericString.toLocaleLowerCase();
+  } catch (e) {
+    return "cannot-slugify";
+  }
 }
